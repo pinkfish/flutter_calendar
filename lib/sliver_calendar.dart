df3808aded46b63a0610776b737599e43ff9c30f
David Bennett <ddb@fb.com>
2018-05-30 13:05:02 -0700

    Add in the example

--- lib/sliver_calendar.dart
+++ lib/sliver_calendar.dart
@@ -1,7 +1,2 @@
-library calendar;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/calendar.dart';
+export 'src/calendarevent.dart';
